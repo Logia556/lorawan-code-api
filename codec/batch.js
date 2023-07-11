28773a446f309cd1e629912a6b967a18e3d0bed7
Logia556 <lorenzo.angileri@isen.yncrea.fr>
2023-07-11 16:23:24 +0200

    capteurs avec un traitement batch sur tago.io rq : certains avaient un semblant d'implémentation codec API

--- codec/batch.js
+++ codec/batch.js
@@ -198,7 +198,6 @@ function createBuffer(byteArray) {
         var sourceBitStart = index;
         var sz = nb_bits - 1;
         if (byteArray.length * 8 < sourceBitStart + nb_bits) {
-            //attibute message in myerror array to send it in debug.js
             myerror.push("Batch : Verify that dest buf is large enough");
 
 
@@ -824,7 +823,6 @@ module.exports = {
 
 };
 
-//export myerror to debug.js
 module.exports.err_msg = myerror;
 
 
