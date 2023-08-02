d755cc3c2d81382363eaddd38d8587031db20aaa
Logia556 <lorenzo.angileri@isen.yncrea.fr>
2023-08-02 10:29:00 +0200

    minimizer qui fonctionne avec les tests jest

--- th_v4/webpack.config.js
+++ th_v4/webpack.config.js
@@ -19,7 +19,36 @@ module.exports={
 
     },
     optimization: {
-            minimize: false,
+        minimizer: [
+            (compiler) => {
+                const TerserPlugin = require('terser-webpack-plugin');
+                new TerserPlugin({
+                    terserOptions: {
+                        compress: {
+                            dead_code: true,
+                            drop_console: false,
+                            drop_debugger: true,
+                            keep_classnames: false,
+                            keep_fargs: true,
+                            keep_fnames: false,
+                            keep_infinity: false
+                        },
+                        mangle: {
+                            eval: false,
+                            keep_classnames: false,
+                            keep_fnames: false,
+                            toplevel: false,
+                            safari10: false
+                        },
+                        module: false,
+                        sourceMap: false,
+                        output: {
+                            comments: 'some'
+                        }
+                    },
+                }).apply(compiler);
+            },
+        ],
 
     },
 }
