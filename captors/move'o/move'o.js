c9eccb27ed72162e3fc4f45083e8b6ba7098b6f9
Logia556 <lorenzo.angileri@isen.yncrea.fr>
2023-08-24 16:37:36 +0200

    move'o.js

--- captors/move'o/move'o.js
+++ captors/move'o/move'o.js
@@ -1,1 +1,11 @@
+let watteco = require("../../codec/decode_uplink")
+
+function decodeUplink(input) {
+
+    return result = watteco.watteco_decodeUplink(input);
+
+}
+
+module.exports.decodeUplink = decodeUplink;
+
 let clusters=["basic:0000","lorawan:8004","configuration:0050","temperature:0402","humidity:0405","binary:000F","occupency:0406","illuminance:0400"]
