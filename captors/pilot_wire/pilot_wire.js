903c7d8a499c6cf21690340b8ae212b1345c9cee
Logia556 <lorenzo.angileri@isen.yncrea.fr>
2023-08-24 16:36:33 +0200

    pilot_wire.js

--- captors/pilot_wire/pilot_wire.js
+++ captors/pilot_wire/pilot_wire.js
@@ -1,1 +1,10 @@
+let watteco = require("../../codec/decode_uplink")
+
+function decodeUplink(input) {
+
+    return result = watteco.watteco_decodeUplink(input);
+
+}
+
+module.exports.decodeUplink = decodeUplink;
 let clusters=["basic:0000","lorawan:8004","configuration:0050","multstateoutput:0013"]
