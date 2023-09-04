2629a3b13fd9f964d5bb70dc51e83a24a665d98b
Logia556 <lorenzo.angileri@isen.yncrea.fr>
2023-09-04 14:36:35 +0200

    ventil'o batch report

--- captors/ventil'O/ventil'O.js
+++ captors/ventil'O/ventil'O.js
@@ -9,7 +9,31 @@ let batch_param = [3, [{taglbl: 0,resol: 1, sampletype: 7,lblname: "mean_differe
     { taglbl: 6, resol: 1, sampletype: 10,lblname: "index", divide: 1},
     { taglbl: 7, resol: 1, sampletype: 1,lblname: "state", divide: 1}]];
 
+function strToDecimalArray(str){
+    let hexArray = [];
+    for (let i=0; i<str.length; i+=2) {
+        hexArray.push(parseInt(str.substring(i, i+2), 16));
+    }
+    return hexArray;
+}
+let argv= process.argv.slice(2);
+
+
+let bytes = [];
+bytes = strToDecimalArray(argv[1]);
+
+let date = argv[2];
+
+let input = {
+    bytes: bytes,
+    fPort: Number(argv[0]),
+    recvTime: date,
+};
+console.log(input)
+
 function decodeUplink(input) {
     return result = watteco.watteco_decodeUplink(input,batch_param);
 }
 module.exports.decodeUplink = decodeUplink;
+let a = decodeUplink(input);
+console.log(a);
