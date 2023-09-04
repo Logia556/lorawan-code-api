8bd21249b9abb32e2e102851e4d0336dd65206d1
Logia556 <lorenzo.angileri@isen.yncrea.fr>
2023-09-04 15:16:05 +0200

    atm'o batch report

--- captors/atm'O/atm'O.js
+++ captors/atm'O/atm'O.js
@@ -11,6 +11,28 @@ let endpointCorresponder={
     count:["index_1","index_2"],
 }
 
+function strToDecimalArray(str){
+    let hexArray = [];
+    for (let i=0; i<str.length; i+=2) {
+        hexArray.push(parseInt(str.substring(i, i+2), 16));
+    }
+    return hexArray;
+}
+let argv= process.argv.slice(2);
+
+
+let bytes = [];
+bytes = strToDecimalArray(argv[1]);
+
+let date = argv[2];
+
+let input = {
+    bytes: bytes,
+    fPort: Number(argv[0]),
+    recvTime: date,
+};
+console.log(input)
+
 function decodeUplink(input) {
     if (input.bytes[2] === 0x00 && input.bytes[3] === 0x0F) {
         return result = watteco.watteco_decodeUplink(input,batch_param,endpointCorresponder);
@@ -18,4 +40,5 @@ function decodeUplink(input) {
     return result = watteco.watteco_decodeUplink(input,batch_param);
 }
 module.exports.decodeUplink = decodeUplink;
-
+let a = decodeUplink(input);
+console.log(a);
