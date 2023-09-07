c0800ba6468c9ff38daea99cc955f1a1148f5e24
Logia556 <lorenzo.angileri@isen.yncrea.fr>
2023-09-07 10:27:22 +0200

    rajout batch params

--- actility/inclin'o_v4/inclin'o.js
+++ actility/inclin'o_v4/inclin'o.js
@@ -1,5 +1,8 @@
 let watteco = require("../../codec/decode_uplink")
-let batch_param=[]
+
+let batch_param=[3, [{ taglbl: 0, resol: 1, sampletype: 6,lblname: "ACCmg", divide: 1},
+    { taglbl: 1, resol: 0.1, sampletype: 12,lblname: "Deg", divide: 1000}]]
+
 let endpointCorresponder={
     analog:["angle"]
 }
