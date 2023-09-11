ea95fb50be039f1b4994b910ad7e1019e6e580be
Logia556 <lorenzo.angileri@isen.yncrea.fr>
2023-09-11 09:28:15 +0200

    ajout endpoint corresponder analog sur lev'o

--- actility/lev'o_v4/lev'o.js
+++ actility/lev'o_v4/lev'o.js
@@ -5,7 +5,9 @@ let batch_param = [3, [{taglbl: 0,resol: 0.004, sampletype: 12,lblname: "delta_p
     { taglbl: 2, resol: 100, sampletype: 6,lblname: "battery_voltage", divide: 1000},
     { taglbl: 3, resol: 100, sampletype: 6,lblname: "external_power_voltage", divide: 1000},
     { taglbl: 4, resol: 1, sampletype: 10,lblname: "index", divide: 1}]];
-let endpointCorresponder={}
+let endpointCorresponder={
+    analog:["delta_pressure","fluid_level"]
+}
 function decodeUplink(input) {
     return result = watteco.watteco_decodeUplink(input,batch_param,endpointCorresponder);
 }
