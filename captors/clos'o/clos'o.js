e8cc2104e17b4539c4aa6716068cca131fad5376
Logia556 <lorenzo.angileri@isen.yncrea.fr>
2023-09-11 16:50:19 +0200

    endpoint corresponder pour th et closo

--- captors/clos'o/clos'o.js
+++ captors/clos'o/clos'o.js
@@ -1,7 +1,9 @@
 let watteco = require("../../codec/decode_uplink")
 
 let batch_param = []
-let endpointCorresponder={}
+let endpointCorresponder={
+    pin_state:["violation_detection","open"]
+}
 function decodeUplink(input) {
     return result = watteco.watteco_decodeUplink(input,batch_param,endpointCorresponder);
 
