4724676c66c6e4bac461b99ddd7cd5d16cbf43ec
Logia556 <lorenzo.angileri@isen.yncrea.fr>
2023-09-11 09:26:20 +0200

    ajout endpoint corresponder analog sur intens'o

--- actility/intens'o_v4/intens'o.js
+++ actility/intens'o_v4/intens'o.js
@@ -5,7 +5,9 @@ let batch_param =[3, [{taglbl: 0,resol: 0.004, sampletype: 12,lblname: "NA", div
     { taglbl: 2, resol: 100, sampletype: 6,lblname: "battery_voltage", divide: 1000},
     { taglbl: 3, resol: 100, sampletype: 6,lblname: "external_power_voltage", divide: 1000},
     { taglbl: 4, resol: 1, sampletype: 10,lblname: "index", divide: 1}]];
-let endpointCorresponder={}
+let endpointCorresponder={
+    analog:["NA","current"]
+}
 function decodeUplink(input) {
     return result = watteco.watteco_decodeUplink(input,batch_param,endpointCorresponder);
 }
