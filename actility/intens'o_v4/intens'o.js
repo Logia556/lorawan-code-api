26b963b3bea87bd2b224016e376f313a825d1d2a
Logia556 <lorenzo.angileri@isen.yncrea.fr>
2023-09-12 11:50:52 +0200

    intso et levo

--- actility/intens'o_v4/intens'o.js
+++ actility/intens'o_v4/intens'o.js
@@ -1,12 +1,9 @@
 let watteco = require("../../codec/decode_uplink")
 
-let batch_param =[3, [{taglbl: 0,resol: 0.004, sampletype: 12,lblname: "NA", divide: 1},
-    { taglbl: 1, resol: 1, sampletype: 12,lblname: "current", divide: 1},
-    { taglbl: 2, resol: 100, sampletype: 6,lblname: "battery_voltage", divide: 1000},
-    { taglbl: 3, resol: 100, sampletype: 6,lblname: "external_power_voltage", divide: 1000},
-    { taglbl: 4, resol: 1, sampletype: 10,lblname: "index", divide: 1}]];
+let batch_param =[2, [{taglbl: 0,resol: 0.1, sampletype: 12,lblname: "4-20_mA", divide: 1},
+    { taglbl: 1, resol: 100, sampletype: 6,lblname: "battery_voltage", divide: 1000}]];
 let endpointCorresponder={
-    analog:["NA","current"]
+    analog:["4-20_mA"]
 }
 function decodeUplink(input) {
     return result = watteco.watteco_decodeUplink(input,batch_param,endpointCorresponder);
