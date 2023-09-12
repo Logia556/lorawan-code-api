755c94afa24409daf0c0d19cf452e7439ca0fb14
Logia556 <lorenzo.angileri@isen.yncrea.fr>
2023-09-12 10:19:23 +0200

    flasho et sppression du dossier captor

--- captors/pilot_wire/pilot_wire.js
+++
@@ -1,8 +0,0 @@
-let watteco = require("../../codec/decode_uplink")
-
-let batch_param=[]
-let endpointCorresponder={}
-function decodeUplink(input) {
-    return result = watteco.watteco_decodeUplink(input, batch_param, endpointCorresponder);
-}
-exports.decodeUplink = decodeUplink;
