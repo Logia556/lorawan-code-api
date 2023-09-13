8fde940d8546172d15cdee1c91452e174b518c57
Logia556 <lorenzo.angileri@isen.yncrea.fr>
2023-09-13 10:35:30 +0200

    flash'o batch

--- actility/flash'o_v4/flash'o.js
+++ actility/flash'o_v4/flash'o.js
@@ -1,6 +1,6 @@
 let watteco = require("../../codec/decode_uplink.js")
 
-let batch_param = [3, [{ taglbl: 0, resol: 1, sampletype: 10,lblname: "index", divide: 1},
+let batch_param = [1, [{ taglbl: 0, resol: 1, sampletype: 10,lblname: "index", divide: 1},
     { taglbl: 1, resol: 100, sampletype: 6,lblname: "battery_voltage", divide: 1000}]];
 
 let endpointCorresponder={}
